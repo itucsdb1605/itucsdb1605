14354273bd2dd20acb3fdcafc0c362ebfa120da8
yildizmert1 <yildizmert1@itu.edu.tr>
2016-10-28 01:00:04 +0300

    Get_elephantSQL_DSN function and running configurations on localhost are added for connection to database.
    Also init_db() function is added for initialization of database.

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
 import datetime
+import json
 import os
+import psycopg2 as dbapi2
+import re
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
-
+from flask.helpers import url_for
+from flask import request
+from init import INIT
 
 app = Flask(__name__)
+
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 
 @app.route('/')
@@ -66,10 +82,25 @@ def topics_page():
     now = datetime.datetime.now()
     return render_template('topics.html', current_time=now.ctime())
 
+@app.route('/initdb')
+def init_db():
+    initialize = INIT(app.config['dsn'])
+    #initialize.All()
+    initialize.universities()
+    initialize.universities_info()
+    return redirect(url_for('home_page'))
+
+
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant' host='localhost' port=5432 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
