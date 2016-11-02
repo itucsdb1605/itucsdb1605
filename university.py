32f207a9df727ac6d0882847981f462c3c2c46a2
yildizmert1 <yildizmert1@itu.edu.tr>
2016-11-02 22:54:54 +0200

    Created university class

---
+++ university.py
@@ -0,0 +1,6 @@
+class University:
+    def __init__(self, title, local, population, type):
+        self.Title = title
+        self.Local = local
+        self.Population = population
+        self.Type = type
