7768022d842eea1f625c93272adda291f986a14b
Halit Uğurgelen <ugurgelen@itu.edu.tr>
2016-11-03 15:32:48 +0200

    Created topics.py for elementary DB operations
    For "Topics" section of our website

---
+++ topics.py
@@ -0,0 +1,38 @@
+import psycopg2 as dbapi2
+
+class topics:
+    def __init__(self, cp):
+        self.cp = cp
+        return
+
+    def get_topiclist(self):
+        with dbapi2.connect(self.cp) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM topics"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            return rows
+            
+    def delete_topic(self, topicID):
+        with dbapi2.connect(self.cp) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM topics WHERE topicID = '%s'" % (topicID)
+            cursor.execute(query)
+            connection.commit()
+            return
+            
+    def add_topic(self, topic, desc):
+        with dbapi2.connect(self.cp) as connection:
+            cursor = connection.cursor()
+            query =  "INSERT INTO topics (topicID, topic, desc) VALUES ('%s','%s','%s')" % (topicID, topic, desc)
+            cursor.execute(query)
+            connection.commit()
+            return
+            
+    def update_topic(self, topicID, topic, desc):
+        with dbapi2.connect(self.cp) as connection:
+            cursor = connection.cursor()
+            query =  "UPDATE topics SET topic = '%s' WHERE topicID='%s', topic='%s', desc='%s'" % (topicID, topic, desc)
+            cursor.execute(query)
+            connection.commit()
+            return
