d35f176e9b4a71e6654abab3cf698a068e65cff6
yildizmert1 <yildizmert1@itu.edu.tr>
2016-11-28 16:29:23 +0200

    Updated add, delete, select and update functions for university table because of new added foreign keys

--- universities.py
+++ universities.py
@@ -17,11 +17,11 @@ class Universities:
     def get_a_universitylist(self):
         with dbapi2.connect(self.cp) as connection:
             cursor = connection.cursor()
-            query = "SELECT * FROM universities_info ORDER BY uni_id ASC"
+            query = "SELECT uni_id, locations.city, locations.country, population, type FROM universities_info JOIN locations ON universities_info.local = locations.loc_id"
             cursor.execute(query)
             rows = cursor.fetchall()
             return rows
-        
+
     def delete_university(self, id):
         with dbapi2.connect(self.cp) as connection:
             cursor = connection.cursor()
@@ -39,7 +39,11 @@ class Universities:
             cursor.execute(query)
             row = cursor.fetchone()
             uni_id = row[0]
-            query =  "INSERT INTO universities_info (uni_id,local,population,type) VALUES ('%s','%s','%s','%s')" % (uni_id,local,population,type)
+            query = "SELECT loc_id FROM locations WHERE city = '%s' " % (local)
+            cursor.execute(query)
+            row = cursor.fetchone()
+            loca_id = row[0]
+            query =  "INSERT INTO universities_info (uni_id,local,population,type) VALUES ('%s','%s','%s','%s')" % (uni_id,loca_id,population,type)
             cursor.execute(query)
             connection.commit()
             return
@@ -52,12 +56,13 @@ class Universities:
             connection.commit()
             return
 
-    def update_a_university(self, id, title, local, population, type):
+    def update_a_university(self, id, uni, city, country, number, type):
         with dbapi2.connect(self.cp) as connection:
             cursor = connection.cursor()
-            query = "UPDATE universities SET title = '%s' WHERE id = '%s'" % (title, id)
+            query = "UPDATE universities SET title = '%s' WHERE id = '%s'" % (uni, id)
             cursor.execute(query)
-            query = "UPDATE universities_info SET local='%s', population='%s', type='%s' WHERE uni_id = '%s'" % (local,population,type,id)
+            #query = "SELECT loc_id FROM locations WHERE city= '%s' " % (city)
+            query = "UPDATE universities_info SET local= 62, population='%s', type='%s' WHERE uni_id = '%d'" % (number,type,id)
             cursor.execute(query)
             connection.commit()
             return
@@ -67,17 +72,17 @@ class Universities:
             return None
         with dbapi2.connect(self.cp) as connection:
             cursor = connection.cursor()
-            query = """SELECT uni.title AS Title, uninf.local AS Local,
+            query = """SELECT uni.title AS Title, loc.city AS City, loc.country AS Country,
                     uninf.population AS Population, uninf.type AS Type
                     FROM
                     universities_info uninf
                     JOIN (SELECT * FROM universities WHERE id = '%s') uni ON uninf.uni_id = uni.id
+                    JOIN (SELECT * FROM locations WHERE loc_id ='%s') loc ON uninf.local = loc.loc_id
                     """ % (id)
             cursor.execute(query)
             row = cursor.fetchone()
             if row is None:
                 return None
-            uni = University(row[0], row[1], row[2], row[3])
+            uni = University(row[0], row[1], row[2], row[3], row[4])
             return uni
-
 
