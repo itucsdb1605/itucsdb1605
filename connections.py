dfe8e99970c7ba027d6c37c081c8ec24929588e3
caglarh <caglarh@itu.edu.tr>
2016-12-18 22:49:26 +0300

    Sorgu satırında yazım hatası düzeltildi.

--- connections.py
+++ connections.py
@@ -8,7 +8,7 @@ class Connections:
     def get_connectionlist(self):
         with dbapi2.connect(self.cp) as connection:
             cursor = connection.cursor()
-            query = "SELECT u1.FirstName AS Mfname, u1.LastName AS Mlname, u2.FirstName AS Ffname, u2.LastName AS Flname FROM connections JOIN users u1 ON connections.MainUserId = u1.UserIdJOIN users u2 ON connections.FriendUserId = u2.UserId"
+            query = "SELECT u1.FirstName AS Mfname, u1.LastName AS Mlname, u2.FirstName AS Ffname, u2.LastName AS Flname FROM connections JOIN users u1 ON connections.MainUserId = u1.UserId JOIN users u2 ON connections.FriendUserId = u2.UserId"
             cursor.execute(query)
             rows = cursor.fetchall()
             return rows
