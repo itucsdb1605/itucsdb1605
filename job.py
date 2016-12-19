48f0db2a3a09ef5c95dc47bea9053b016da02ed4
Mustafa ÇOBAN <cobanmus15@itu.edu.tr>
2016-12-19 06:02:37 +0200

    updated job methods

--- job.py
+++ job.py
@@ -1,63 +1,77 @@
 import psycopg2 as dbapi2
 
 class Job:
-    def __init__(self, cp, id = None, company = None, position = None, salary = None):
+    def __init__(self, cp, id = None, companyId = None, position = None, salary = None):
         self.cp = cp
         self.id = id
-        self.company = company
+        self.companyId = companyId
         self.position = position
         self.salary = salary
-        
+
     def set_id(self, id):
         self.id = id
-        
-    def set_company(self, company):
-        self.company = company
-    
+
+    def set_companyId(self, companyId):
+        self.companyId = companyId
+
     def set_position(self, position):
         self.position = position
-    
+
     def set_salary(self, salary):
-        self.salary = salary    
-    
+        self.salary = salary
+
+
+    def get_job(self,id):
+        with dbapi2.connect(self.cp) as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute("""
+                                    SELECT T1.ID, COMPANIES.TITLE, T1.POSITION, T1.SALARY FROM (SELECT * FROM JOBS WHERE (ID={})) AS T1
+                                    INNER JOIN COMPANIES
+                                    ON COMPANIES.ID=T1.ID
+                                """.format(id))
+                    job_list = cursor.fetchall()
+                    print(job_list)
+                    return job_list
+
     def get_jobs(self):
-        if self.id is None:  
-            with dbapi2.connect(self.cp) as connection:
-                with connection.cursor() as cursor:     
-                    cursor.execute("""SELECT * FROM JOBS""")    
-                    job_list = cursor.fetchall() 
-                    return job_list
-        else:
-            with dbapi2.connect(self.cp) as connection:
-                with connection.cursor() as cursor:     
-                    cursor.execute("""SELECT * FROM JOBS WHERE (ID = {})""".format(self.id))    
-                    job_list = cursor.fetchall() 
-                    return job_list
-                
-    
+        with dbapi2.connect(self.cp) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                                    SELECT T1.ID, COMPANIES.TITLE, T1.POSITION, T1.SALARY FROM (SELECT * FROM JOBS) AS T1
+                                    INNER JOIN COMPANIES
+                                    ON COMPANIES.ID=T1.ID
+                                    """)
+                job_list = cursor.fetchall()
+                print(job_list)
+                return job_list
+
+
+
     def add_job(self):
         with dbapi2.connect(self.cp) as connection:
-            with connection.cursor() as cursor:   
-                statement = """INSERT INTO JOBS(CompanyName, Position, Salary) VALUES
-                        ( '{}', '{}', '{}' );
-                    """.format(self.company, self.position, self.salary)
-                cursor.execute(statement) 
-                
+            with connection.cursor() as cursor:
+                print("""INSERT INTO JOBS(CompanyId, Position, Salary) VALUES
+                        ( {}, '{}', {} )
+                    """.format(self.companyId, self.position, self.salary))
+                statement = """INSERT INTO JOBS(CompanyId, Position, Salary) VALUES
+                        ( {}, '{}', {} )""".format(self.companyId, self.position, self.salary)
+                cursor.execute(statement)
+
     def delete_jobs(self, ids):
         with dbapi2.connect(self.cp) as connection:
-            with connection.cursor() as cursor:   
+            with connection.cursor() as cursor:
                 statement = """DELETE FROM JOBS WHERE ID = {}"""
                 for id in ids:
                     id = id.split('/', maxsplit=1)
                     id = id[0]
                     print(id)
                     cursor.execute(statement.format(id))
-    
+
     def update_job(self):
         with dbapi2.connect(self.cp) as connection:
-            with connection.cursor() as cursor:   
+            with connection.cursor() as cursor:
                 statement = """UPDATE JOBS
-                    SET  CompanyName='{}', Position='{}' ,Salary={}     
-                    WHERE ID={};""".format(self.company, self.position, self.salary, self.id)
-                cursor.execute(statement)    
-                
+                    SET  CompanyId={}, Position='{}' ,Salary={}
+                    WHERE ID={};""".format(self.companyId, self.position, self.salary, self.id)
+                cursor.execute(statement)
+
